08c936e4424b2e85c79c77222c88fffecd0bf862
pepyn <160941912+Phepyn1@users.noreply.github.com>
2024-08-16 19:17:53 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* WIFI_SSID = "motorola one 7436";
-const char* WIFI_PASSWORD = "12345678";
-const char* BROKER_IP = "192.168.43.26";
-const int BROKER_PORT = 1883;
+const char* WIFI_SSID = "seu_ssid";
+const char* WIFI_PASSWORD = "sua_senha";
+const char* BROKER_IP = "ip_do_broker";
+const int BROKER_PORT = "porta_do_broker";
 
 #endif
